166092e2a7e50a88d99a6204aa298f91d6797b63
hika.lichengfeng <hika.lichengfeng@fenda.com>
2023-02-02 17:54:16 +0800

    init

---
+++ src/observe/meta_property/observe.js
@@ -0,0 +1,12 @@
+const descriptor = {
+  enumerable: true,
+  configurable: true
+}
+
+const observe = (value, get, set, options) => {
+  const {
+    deep
+  } = options
+}
+
+export default observe
