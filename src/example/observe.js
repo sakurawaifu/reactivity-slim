1640917e0f47a16a601a84c92bcda02bc7edf906
hika.lichengfeng <hika.lichengfeng@fenda.com>
2023-02-03 14:41:20 +0800

    实现初版的 meta_property

---
+++ src/example/observe.js
@@ -0,0 +1,21 @@
+import { observable, observe } from '../observe/meta_property/observe.js'
+
+const data = {
+  a: 1,
+  b: 2,
+  c: {
+    c1: {
+      c11: {
+        c111: 1
+      }
+    }
+  }
+}
+
+observable(data)
+
+observe(data, 'c.c1', () => {
+  console.log('update c.c1')
+})
+
+data.c.c1 = 2
--- src/observe/meta_property/observe.js
+++ src/observe/meta_property/observe.js
@@ -1,12 +1,56 @@
-const descriptor = {
-  enumerable: true,
-  configurable: true
-}
+import { isPlainObject } from '../../utils/utils.js'
 
-const observe = (value, get, set, options) => {
+const depCenter = new WeakMap()
+
+const observable = (obj, options = {}) => {
   const {
-    deep
+    compare = true,
+    deep = true
   } = options
+
+  const keyMap = {}
+  depCenter.set(obj, keyMap)
+
+  for (const key in obj) {
+    let value = obj[key]
+    const deps = new Set()
+    keyMap[key] = deps
+
+    Object.defineProperty(obj, key, {
+      enumerable: true,
+      configurable: true,
+      get() {
+        return value
+      },
+      set(nv) {
+        if (compare && nv === value) return
+
+        deps.forEach(dep => dep())
+        nv = value
+      }
+    })
+
+    if (deep) {
+      if (isPlainObject(value)) {
+        observable(value, options)
+      }
+    }
+  }
 }
 
-export default observe
+const observe = (obj, key, callback) => {
+  let targetObj = obj
+  let targetKey = key
+  if (key.includes('.')) {
+    const keyPath = key.split('.')
+    targetKey = keyPath.pop()
+    targetObj = keyPath.reduce((acc, v) => acc[v], obj)
+  }
+
+  depCenter.get(targetObj)[targetKey].add(callback)
+}
+
+export {
+  observable,
+  observe
+}
